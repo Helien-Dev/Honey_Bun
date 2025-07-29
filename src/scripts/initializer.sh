c283db9e16255ef40bc0cd72babf4905d6e8cea3
Alvaro Avila <alvaroaviladev@gmail.com>
2025-07-29 14:20:04 -0500

    test tailwindcss reset

--- src/scripts/initializer.sh
+++ src/scripts/initializer.sh
@@ -5,6 +5,5 @@
 python src/manage.py collectstatic --noinput
 python src/manage.py makemigrations
 python src/manage.py migrate
-# python manage.py runserver 0.0.0.0:8000
+python src/manage.py runserver 0.0.0.0:8000
 
-./src/scripts/tailwindcss.sh
--- src/scripts/wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./src/scripts/initializer.sh
-./src/scripts/tailwindcss.sh
-
-wait -n
